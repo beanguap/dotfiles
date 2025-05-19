e06859136d999db430c20ef8757b1215b5b8490f
Beanguap <martinez.jeriel01@gmail.com>
2025-05-19 19:23:51 -0400

    Update Nvim configs

--- .config/nvim/lua/jeri/plugins/lsp/mason.lua
+++ .config/nvim/lua/jeri/plugins/lsp/mason.lua
@@ -2,18 +2,13 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
-     "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
-    -- import mason
     local mason = require("mason")
-
-      local mason_tool_installer = require("mason-tool-installer")
-
-    -- import mason-lspconfig
+    local mason_tool_installer = require("mason-tool-installer")
     local mason_lspconfig = require("mason-lspconfig")
 
-    -- enable mason and configure icons
     mason.setup({
       ui = {
         icons = {
@@ -25,9 +20,7 @@ return {
     })
 
     mason_lspconfig.setup({
-      -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
         "html",
         "cssls",
         "tailwindcss",
@@ -40,17 +33,15 @@ return {
       },
     })
 
-        mason_tool_installer.setup({
+    mason_tool_installer.setup({
       ensure_installed = {
         "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-         "pylint", -- python linter
+        "stylua",   -- lua formatter
+        "isort",    -- python formatter
+        "black",    -- python formatter
+        "pylint",   -- python linter
         "eslint_d", -- js linter
       },
-    }}
-
-
+    })
   end,
 }
---
+++ .zshrc
@@ -0,0 +1,54 @@
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# Path to Oh My Zsh
+export ZSH="$HOME/.oh-my-zsh"
+
+# Set your preferred theme
+ZSH_THEME="robbyrussell"  # You can switch to "agnoster", "bira", or "powerlevel10k" if installed
+
+# Plugins
+plugins=(
+  git
+  z
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# Starship prompt
+eval "$(starship init zsh)"
+
+# Bun setup
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# General PATH additions (add more as needed)
+export PATH="$HOME/.local/bin:$PATH"
+
+# Aliases
+alias l='ls -lah'
+alias gs='git status'
+alias gc='git commit'
+alias gp='git push'
+alias gl='git log --oneline --graph --decorate'
+alias nv='nvim'
+alias update='sudo pacman -Syu && yay -Syu'
+
+# Terminal colors
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
+# History settings
+export HISTSIZE=5000
+export SAVEHIST=5000
+setopt hist_ignore_dups
+setopt share_history
+
+# Syntax highlighting settings
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+
+# Disable "last login" message
+touch ~/.hushlogin
+
